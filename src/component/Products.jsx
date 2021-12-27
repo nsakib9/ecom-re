5b9e821165ff511d6a1b858e91f115a1b815d1d1
nsakib9 <sakibkhan207@gmail.com>
2021-12-27 08:24:40 +0600

    Product.jsx update 1

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Products = () => {
+
+    const [data,setData] = useState([]);
+    cosnt [filter, setFilter] = useState(data);
+    const [loading, setLoading] = useState(false);
+    let componentMounted = true;
+
     return (
         <div>
             
