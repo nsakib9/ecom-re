f7dddc060d6b96c9f7ad2583603725ee57ed3116
nsakib9 <sakibkhan207@gmail.com>
2021-12-27 23:42:20 +0600

    App: import {Switch, Route} adn wrap routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 
 import Navbar from './component/Navbar';
 import Home from './component/Home';
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
+    <Navbar /> 
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/products" component={Products} />
+    </Switch>
     </>
   );
 }
