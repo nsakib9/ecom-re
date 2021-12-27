d2a22e918c949130505537b9e8dd2081932b8bb0
nsakib9 <sakibkhan207@gmail.com>
2021-12-27 12:08:59 +0600

    P: Update ShowProducts = () => method

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,62 +1,62 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
 const Products = () => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
+  let componentMounted = true;
 
-    const [data,setData] = useState([]);
-    const [filter, setFilter] = useState(data);
-    const [loading, setLoading] = useState(false);
-    let componentMounted = true;
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (componentMounted) {
+        setData(await response.clone().json());
+        setFilter(await response.json());
+        setLoading(false);
+        console.log(filter);
+      }
+      return () => {
+        componentMounted = false;
+      };
+    };
+    getProducts();
+  }, []);
 
-    useEffect(() => {
-        const getProducts = async () => {
-            setLoading(true);
-            const response = await fetch("https://fakestoreapi.com/products");
-            if(componentMounted){
-                setData(await response.clone().json());
-                setFilter(await response.json());
-                setLoading(false);
-                console.log(filter)
-            }
-            return () => {
-                componentMounted = false;
-            }
-        }
-        getProducts();
-    }, []);
-
-    const Loading = () => {
-        return(
-            <>
-            Loading ....
-            </>
-        )
-    }
-    const ShowProducts = () => {
-        return(
-            <>
-            <div className="buttons">
-                <button className="btn btn-outline-dark">All</button>
-                <button className="btn btn-outline-dark">Men's Clothing</button>
-            </div>
-            </>
-        )
-    }
-
+  const Loading = () => {
+    return <>Loading ....</>;
+  };
+  const ShowProducts = () => {
     return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h1>Latest products</h1>
-                        <hr />
-                    </div>
-                </div>
-                <div className="row justify-content-center">
-                    {loading ? <Loading /> : <ShowProducts />}
-                </div>
-            </div>
+      <>
+        <div className="buttons d-flex justify-content-center mb-5 pb-5">
+          <button className="btn btn-outline-dark me-2">All</button>
+          <button className="btn btn-outline-dark me-2">Men's Clothing</button>
+          <button className="btn btn-outline-dark me-2">
+            Women's Clothing
+          </button>
+          <button className="btn btn-outline-dark me-2">Jewelery </button>
+          <button className="btn btn-outline-dark me-2">Electronics</button>
         </div>
+      </>
     );
-}
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>Latest products</h1>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <Loading /> : <ShowProducts />}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Products;
