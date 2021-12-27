ccb992f0b1e2edbc18c4f99c6141d79e4c7070b0
nsakib9 <sakibkhan207@gmail.com>
2021-12-27 08:46:59 +0600

    p4: set json with componentMounted and return

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -10,7 +10,15 @@ const Products = () => {
     useEffect(() => {
         const getProducts = async () => {
             setLoading(true);
-            const response = await fetch("https://fakestoreapi.com/products")
+            const response = await fetch("https://fakestoreapi.com/products");
+            if(componentMounted){
+                setData(await response.clone().json());
+                setFilter(await response.json());
+                setLoading(false);
+            }
+            return () => {
+                componentMounted = false;
+            }
         }
         getProducts();
     }, [])
