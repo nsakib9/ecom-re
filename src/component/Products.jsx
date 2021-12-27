87c122e484d49ac8cb35bcf11fd80a13077745cd
nsakib9 <sakibkhan207@gmail.com>
2021-12-27 23:13:34 +0600

    P: product.title.substring(0,12)

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -45,7 +45,7 @@ const Products = () => {
                 <div className="card h-100 text-center p-4" key={product.id}>
                     <img src={product.image} className="card-img-top" alt={product.title} />
                         <div className="card-body">
-                            <h5 className="card-title">{product.title}</h5>
+                            <h5 className="card-title mb-0">{product.title.substring(0,12)}</h5>
                             <p className="card-text">${product.price}</p>
                             <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
