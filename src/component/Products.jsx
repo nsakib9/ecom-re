244315774ce7259cf94b7900333cc72d0bb3a8ae
nsakib9 <sakibkhan207@gmail.com>
2022-01-11 14:37:44 +0600

    5-Dynamic value and design update filter.map sec..

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -42,20 +42,15 @@ const Products = () => {
           return (
             <>
               <div className="col-md-3">
-                <div className="card" style="width: 18rem;">
-                  <img src="..." className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" className="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
+              <div className="card h-100 text-center p-4" key={product.id}>
+                    <img src={product.image} className="card-img-top" alt={product.title} height="250px" />
+                        <div className="card-body">
+                            <h5 className="card-title mb-0">{product.title.substring(0,12)}...</h5>
+                            <p className="card-text lead fw-bold">${product.price}</p>
+                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                        </div>
                 </div>
-              </div>
+</div>
             </>
           );
         })}
