9a4174d758c1e9baa9e1cc2faaaedc4614f341c5
nsakib9 <sakibkhan207@gmail.com>
2022-01-11 22:49:58 +0600

    Prt: Design single product page with dynamic data

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {useParams } from 'react-router'
+import { NavLink } from 'react-router-dom';
 
 const Product = () => {
     const {id} = useParams();
@@ -28,13 +29,33 @@ const Product = () => {
         <div className="col-md-6">
             <img src={product.image} alt={product.title} height="400px" width="400px" />
         </div>
+        <div className="col-md-6">
+            <h4 className="text-uppercase text-black-50">
+                {product.category}
+            </h4>
+            <h1 className='display-5'>{product.title}</h1>
+            <p className="lead fw-bolder">
+                Rating {product.rating && product.rating.rate}
+                <i className="fa fa-star"></i>
+            </p>
+            <h3 className="display-6 fw-bold my-4">
+                $ {product.price}
+            </h3>
+            <p className="lead">{product.description}</p>
+            <button className="btn btn-outline-dark px-4 py-2">
+                Add to cart
+            </button>
+            <NavLink to="/cart" className="btn btn-outline-dark ms-2 px-3 py-2">
+                Go to cart
+            </NavLink>
+        </div>
         </>
         )
     }
     return (
         <div>
-            <div className="container">
-                <div className="row">
+            <div className="container py-5">
+                <div className="row py-4">
                     {loading ? <Loading /> : <ShowProduct />}
                 </div>
             </div>
