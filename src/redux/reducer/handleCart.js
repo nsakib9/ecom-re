b878ed9d2d48f169ca1fb795f7bffdd821ee5626
nsakib9 <sakibkhan207@gmail.com>
2022-01-13 08:15:18 +0600

    handleCart(DELITEM), store.js, reducer>index.js

--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -21,6 +21,17 @@ const handleCart = (state = cart, action) => {
         ];
       }
       break;
+      case "DELITEM":
+          const exist1 = state.find((x) => x.id ===product.id);
+          if(exist1.qty === 1){
+              return state.filter((x) => x.id !== exist1.id);
+          }else{
+              return state.map((x)=>
+                x.id = product.id ? {...x, qty: x.qty -1} : x
+              );
+          }
+          break;
+
 
     default:
       break;
